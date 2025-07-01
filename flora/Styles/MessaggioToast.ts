4c2d2cf7d0fae202381c547a6ce0c9751572f618
Carlo <c.cesarano18@studenti.unisa.it>
2025-07-01 15:06:52 +0200

    Aggiunto Style a MessaggioToast

---
+++ flora/Styles/MessaggioToast.ts
@@ -0,0 +1,23 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+    toast: {
+        position: 'absolute',
+        top: 80,
+        zIndex: 1000,
+        elevation: 1000,
+    },
+
+    contenuto: {
+        paddingHorizontal: 15,
+    },
+
+    titolo: {
+        fontSize: 18,
+        fontWeight: '400',
+    },
+
+    messaggio: {
+        fontSize: 14,
+    },
+})
