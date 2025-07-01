1ef1551612bf543270e18f2e64a9d292c7e5cfc3
Carlo <c.cesarano18@studenti.unisa.it>
2025-07-01 17:52:25 +0200

    ...

--- flora/Schermate/Home.tsx
+++ flora/Schermate/Home.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import {NativeStackScreenProps} from '@react-navigation/native-stack';
-import {RootStackParamList} from '../types';
 // COMPONENTI NATIVI
 import {ScrollView} from 'react-native';
 // COMPONENTI CUSTOM
 import Background from '../Componenti/Comuni/Background';
 import NavBar from '../Componenti/Comuni/NavBar';
 
-type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
-
-export default function Home({navigation}: Props) {
+export default function Home() {
     return (
         <Background>
             <NavBar/>
