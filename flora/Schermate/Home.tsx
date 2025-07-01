c671f8e658c499d2497918c0924cadda1bef2390
Carlo <c.cesarano18@studenti.unisa.it>
2025-07-01 17:51:57 +0200

    Impostato lo scheletro della schermata Home

--- flora/Schermate/Home.tsx
+++ flora/Schermate/Home.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {RootStackParamList} from '../types';
+// COMPONENTI NATIVI
+import {ScrollView} from 'react-native';
 // COMPONENTI CUSTOM
 import Background from '../Componenti/Comuni/Background';
 import NavBar from '../Componenti/Comuni/NavBar';
@@ -11,6 +13,17 @@ export default function Home({navigation}: Props) {
     return (
         <Background>
             <NavBar/>
+
+            <ScrollView>
+                {/*
+                <UltimePianteAggiunte/>
+                <ProssimiInterventi/>
+                */}
+            </ScrollView>
+
+            {/*
+            <AggiungiPiantaButton/>
+            */}
         </Background>
     )
 }
