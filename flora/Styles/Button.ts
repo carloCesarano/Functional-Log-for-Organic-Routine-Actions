6098383ecdc5638f9b0d3e9df363382015a9cd8f
lorenzocibellis <l.cibellis@studenti.unisa.it>
2025-07-05 13:25:35 +0200

    rimesso button.ts vecchio

--- flora/Styles/Button.ts
+++ flora/Styles/Button.ts
@@ -2,28 +2,17 @@ import {StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
     button: {
-        width: '35%',
-        height: 60,
+        width: '100%',
+        height: 44,
+        backgroundColor: '#FFF',
         justifyContent: 'center',
-        borderRadius: 30,
+        borderRadius: 8,
         paddingHorizontal: 12,
         borderWidth: 0,
-        alignItems: 'center',
-        backgroundColor: '#52B04E',
-        marginRight: 20,
     },
 
     testo: {
-        fontSize: 25,
-        color: 'white',
-    },
-
-    buttonContainer: {
-        flexDirection: 'row',
-        bottom: 60,
-        justifyContent: 'space-between',
-        marginRight: 60,
-
+        fontSize: 16,
+        color: 'BLACK',
     }
-
 })
