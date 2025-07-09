fea8389962e642f481cc5a05ae9c413ee201c517
Carlo <c.cesarano18@studenti.unisa.it>
2025-07-09 15:33:12 +0200

    Centralizzazione del Titolo in Specie

--- flora/Schermate/Specie.tsx
+++ flora/Schermate/Specie.tsx
@@ -1,18 +1,16 @@
-import React from "react";
-import NavBar from "../Componenti/Comuni/NavBar";
-import Background from "../Componenti/Comuni/Background";
-import ListaSpecie from "../Componenti/Schermate/Specie/ListaSpecie";
-import Titolo from "../Componenti/Comuni/Titolo";
-import {View} from "react-native";
+import {JSX} from 'react';
+// COMPONENTI CUSTOM
+import NavBar      from '../Componenti/Comuni/NavBar';
+import Background  from '../Componenti/Comuni/Background';
+import ListaSpecie from '../Componenti/Schermate/Specie/ListaSpecie';
+import Titolo from '../Componenti/Comuni/Titolo';
 
-export default function Specie() {
+export default function (): JSX.Element {
     return (
         <Background>
-            <NavBar />
-            <View style={{ width: "100%", alignItems: "flex-start" }}>
-                <Titolo nome="Specie" />
-            </View>
-            <ListaSpecie />
+            <NavBar/>
+            <Titolo nome='Specie'/>
+            <ListaSpecie/>
         </Background>
     );
 }
