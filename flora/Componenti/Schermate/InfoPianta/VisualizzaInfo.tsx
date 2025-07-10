b4db4a9f72e6cd35a4047bb4f310f5f8b1ce95bf
Carlo <c.cesarano18@studenti.unisa.it>
2025-07-10 16:54:43 +0200

    Inserita logica dei pulsanti all'interno di VisualizzaInfo

--- flora/Componenti/Schermate/InfoPianta/VisualizzaInfo.tsx
+++ flora/Componenti/Schermate/InfoPianta/VisualizzaInfo.tsx
@@ -1,17 +1,27 @@
-import {JSX, useCallback, useState} from 'react';
-import {FlatList, ScrollView, Text} from 'react-native';
-import {PiantaPosseduta} from '../../../Model/PiantaPosseduta';
-import * as PiantePosseduteDAO from '../../../Database/PiantePosseduteDAO';
-import {useFocusEffect} from '@react-navigation/native';
+import {Dispatch, JSX, SetStateAction, useCallback, useState} from 'react';
+import {useFocusEffect, useNavigation} from '@react-navigation/native';
+import {StackNavigationProp} from "@react-navigation/stack";
+import {RootStackParamList} from "../../../types";
+// COMPONENTI NATIVI
+import {FlatList, View, Text, Alert} from 'react-native';
+// COMPONENTI CUSTOM
 import ImmaginePianta from './ImmaginePianta';
-import InfoIntervento from "./InfoIntervento";
+import InfoIntervento from './InfoIntervento';
+import Button from '../../Comuni/Input/Button';
+// UTILITY
+import * as PiantePosseduteDAO from '../../../Database/PiantePosseduteDAO';
+import {PiantaPosseduta} from '../../../Model/PiantaPosseduta';
 
 interface Props {
-    id: number
+    id: number,
+    edit: Dispatch<SetStateAction<boolean>>
 }
 
-export default function ({id}: Props): JSX.Element {
+export default function ({id, edit}: Props): JSX.Element {
     const [pianta, setPianta] = useState<PiantaPosseduta | null>(null)
+
+    // HOOKS
+    const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
 
     useFocusEffect(useCallback(() => {
         const caricaDati = async () => {
@@ -24,52 +34,92 @@ export default function ({id}: Props): JSX.Element {
         <Text
             style={{backgroundColor: '#67d362', borderRadius: 8, padding: 8}}
         >{item}</Text>
-    )
+    );
+
+    const eliminaPianta = () => {
+        if (!pianta) return;
+
+        async function confermaElimina() {
+            if (pianta === null) return;
+            await PiantePosseduteDAO.remove(pianta);
+            navigation.navigate('ListaPiante', {cercato: ''})
+        }
+        Alert.alert(
+            'Conferma eliminazione',
+            'Vuoi davvero eliminare ' + pianta?.getNome() + '?',
+            [
+                {
+                    style: 'cancel',
+                    text: 'No'
+                },
+                {
+                    style: 'destructive',
+                    text: 'Sì',
+                    onPress: confermaElimina
+                }
+            ]
+        )
+    }
 
     return (
-        <ScrollView
-            style={{width: '90%', borderRadius: 25, borderStyle: 'solid', borderWidth: 3, backgroundColor: 'white'}}
-            contentContainerStyle={{width: '100%'}}
-        >
-            <ImmaginePianta pianta={pianta}/>
-            <Text
-                numberOfLines={1}
-                ellipsizeMode='tail'
-                style={{textAlign: 'center', marginTop: 12, fontWeight: '900', fontSize: 26}}>
-                {pianta?.getNome()}</Text>
-            <Text
-                numberOfLines={1}
-                ellipsizeMode='tail'
-                style={{textAlign: 'center', marginTop: 8, fontSize: 18, fontStyle: 'italic'}}>
-                {pianta?.getSpecie().getSpecie()}</Text>
-            <Text
-                numberOfLines={1}
-                ellipsizeMode='tail'
-                style={{textAlign: 'center', marginTop: 8, fontSize: 18}}>
-                Acquisita il: {pianta?.getAcq().toLocaleDateString()}</Text>
+        <>
+            <View
+                style={{width: '90%', borderRadius: 25, borderStyle: 'solid', borderWidth: 3, backgroundColor: 'white', overflow: 'hidden'}}
+            >
+                <ImmaginePianta pianta={pianta}/>
+                <Text
+                    numberOfLines={1}
+                    ellipsizeMode='tail'
+                    style={{textAlign: 'center', marginTop: 12, fontWeight: '900', fontSize: 26}}>
+                    {pianta?.getNome()}</Text>
+                <Text
+                    numberOfLines={1}
+                    ellipsizeMode='tail'
+                    style={{textAlign: 'center', marginTop: 8, fontSize: 18, fontStyle: 'italic'}}>
+                    {pianta?.getSpecie().getSpecie()}</Text>
+                <Text
+                    numberOfLines={1}
+                    ellipsizeMode='tail'
+                    style={{textAlign: 'center', marginTop: 8, fontSize: 18}}>
+                    Acquisita il: {pianta?.getAcq().toLocaleDateString()}</Text>
 
-            <FlatList
-                data={pianta?.getCategorie()}
-                horizontal={true}
-                style={{marginTop: 12}}
-                contentContainerStyle={{paddingHorizontal: 16, gap: 16}}
-                keyExtractor={c => c}
-                renderItem={renderCategoria}/>
+                <FlatList
+                    data={pianta?.getCategorie()}
+                    horizontal={true}
+                    style={{marginTop: 12}}
+                    contentContainerStyle={{paddingHorizontal: 16, gap: 16}}
+                    keyExtractor={c => c}
+                    renderItem={renderCategoria}/>
 
-            <InfoIntervento
-                pianta={pianta}
-                tipo='INN'
-                setPianta={setPianta}/>
+                <InfoIntervento
+                    pianta={pianta}
+                    tipo='INN'
+                    setPianta={setPianta}/>
 
-            <InfoIntervento
-                pianta={pianta}
-                tipo='POT'
-                setPianta={setPianta}/>
+                <InfoIntervento
+                    pianta={pianta}
+                    tipo='POT'
+                    setPianta={setPianta}/>
 
-            <InfoIntervento
-                pianta={pianta}
-                tipo='RINV'
-                setPianta={setPianta}/>
-        </ScrollView>
+                <InfoIntervento
+                    pianta={pianta}
+                    tipo='RINV'
+                    setPianta={setPianta}/>
+            </View>
+
+            <View
+                style={{ flexDirection: 'row', justifyContent: 'space-between', width: '80%', gap: 16, marginTop: 18 }}>
+                <Button
+                    testo='Elimina'
+                    stileButton={{width: '50%',height: 60, borderRadius: 18, backgroundColor: 'red',}}
+                    stileTesto={{textAlign: 'center', fontSize: 24, color: 'white', fontWeight: 'bold',}}
+                    onPress={eliminaPianta}/>
+                <Button
+                    testo='Modifica'
+                    stileButton={{width: '50%',height: 60, borderRadius: 18, backgroundColor: '#30a505',}}
+                    stileTesto={{textAlign: 'center', fontSize: 24, color: 'white', fontWeight: 'bold',}}
+                    onPress={() => edit(true)}/>
+            </View>
+        </>
     )
 }
--- flora/Schermate/InfoPianta.tsx
+++ flora/Schermate/InfoPianta.tsx
@@ -2,14 +2,12 @@ import {JSX, useState} from 'react';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {RootStackParamList} from '../types';
 // COMPONENTI NATIVI
-import {View} from 'react-native';
+import {ScrollView} from 'react-native';
 // COMPONENTI CUSTOM
 import Background from '../Componenti/Comuni/Background';
 import NavBar     from '../Componenti/Comuni/NavBar';
-import Titolo     from '../Componenti/Comuni/Titolo';
-import FormModifica from '../Componenti/Schermate/InfoPianta/FormModifica';
+import FormModifica   from '../Componenti/Schermate/InfoPianta/FormModifica';
 import VisualizzaInfo from '../Componenti/Schermate/InfoPianta/VisualizzaInfo';
-import Button from '../Componenti/Comuni/Input/Button';
 
 // DEFINISCO GLI INPUT
 type Props = NativeStackScreenProps<RootStackParamList, 'InfoPianta'>;
@@ -18,42 +16,20 @@ export default function ({route}: Props): JSX.Element {
     const {ID} = route.params;
     const [editing, setEditing] = useState<boolean>(false);
 
-    const editMode = () => {
-        setEditing(true);
-    }
-
-    const salvaEdit = () => {
-        setEditing(false);
-    }
-
     return (
         <Background>
             <NavBar/>
-            <Titolo nome="InfoPianta"/>
+            <ScrollView
+                contentContainerStyle={{justifyContent: 'center', alignItems: 'center', paddingBottom: 18}}>
 
-            {!editing && (
-                <VisualizzaInfo id={ID}/>
-            )}
-
-            {editing && (
-                <FormModifica id={1}/>
-            )}
-
-            <View style={{flexDirection: 'row', justifyContent: 'space-evenly', width: '100%', marginTop: 20}}>
-                <Button testo='Elimina' stileButton={{width: '30%'}}/>
                 {!editing && (
-                    <Button
-                        testo='Modifica'
-                        onPress={editMode}
-                        stileButton={{width: '30%'}}/>
+                    <VisualizzaInfo id={ID} edit={setEditing}/>
                 )}
+
                 {editing && (
-                    <Button
-                        testo='Salva'
-                        onPress={salvaEdit}
-                        stileButton={{width: '30%'}}/>
+                    <FormModifica id={1}/>
                 )}
-            </View>
+            </ScrollView>
 
         </Background>
     )
